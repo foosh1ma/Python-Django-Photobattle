6626637a56e5980d96f665e41d4fab09d8277ba0
foosh1ma <pasha.prov3@gmail.com>
2022-09-30 00:39:21 +0300

    adding models, main template

--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
