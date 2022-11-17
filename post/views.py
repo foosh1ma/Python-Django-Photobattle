d5c13e7319bfdf7b854eecfc58adf10981ac76ae
foosh1ma <pasha.prov3@gmail.com>
2022-11-18 02:46:22 +0300

    creating post details page, first view, new template

--- post/views.py
+++ post/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 
-# Create your views here.
 def index(request):
     return render(request, 'post/index.html')
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/post_detail.html', locals())
