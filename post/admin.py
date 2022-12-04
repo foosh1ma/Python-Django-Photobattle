bd18e6ee1b471d26555c905a3d480054d7e5cdf7
foosh1ma <pasha.prov3@gmail.com>
2022-12-04 04:42:52 +0300

    models changes

--- post/admin.py
+++ post/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 
-admin.register(User)
-admin.register(Post)
+admin.site.register(User)
+admin.site.register(Post)
--- post/models.py
+++ post/models.py
@@ -9,9 +9,24 @@ class User(models.Model):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='image/', height_field=None, width_field=None, max_length=100,
+                              verbose_name='image')
+    likes = models.ManyToManyField(User, related_name='likes')
+
+    def get_likes(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_for = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'Comment for post: {self.post_for.name}. From: {self.owner.name}'
